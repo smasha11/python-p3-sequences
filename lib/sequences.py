8bc5a7da362e72e622637e6aeaced5db4021c8c3
smasha11 <ismail.hussein.1120@gmail.com>
2024-08-29 15:01:24 +0300

    update

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+    elif length == 2:
+        print([0, 1])
+        return
+
+    fib_seq = [0, 1]
+    for i in range(2, length):
+        fib_seq.append(fib_seq[-1] + fib_seq[-2])
+
+    print(fib_seq)
